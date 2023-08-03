b3e291aae1663bc3bd3bb5ab94aab1b840f9cd77
Raghav Goel <98101326+goelraghav002@users.noreply.github.com>
2023-08-03 10:55:43 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string name;
+    cout << "Enter your name : ";
+    cin >> name;
+    cout << "Hi " << name << ", welcome to github";
+    return 0;
+}
